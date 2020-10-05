be180d1c42f0556aba63ddc28d197824b5c6a621
imperative-scientist-4600 <nelom3@gmail.com>
2020-10-05 12:23:18 +0000

    Automatically backed up by Learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -1,14 +1,39 @@
-class Backer
-  attr_reader :name, :backed_projects
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# class Backer
+#   attr_reader :name, :backed_projects
   
-  def initialize(name)
-    @name = name
-    @backed_projects = []
-  end
+#   def initialize(name)
+#     @name = name
+#     @backed_projects = []
+#   end
   
-  def back_project(project)
-    backed_projects << project
-    project.backers << self
-  end
+#   def back_project(project)
+#     backed_projects << project
+#     project.backers << self
+#   end
   
-end
+# end
